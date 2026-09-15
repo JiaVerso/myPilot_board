d6160cd4df973f30e144bb73a4fee6d0d305d51c
Jack Shaw <2870998241@qq.com>
2026-09-15 19:26:55 +0800

    update: first commit myPilot_board

--- bsp/stm32/myPilot_board/rtconfig.py
+++ bsp/stm32/myPilot_board/rtconfig.py
@@ -17,7 +17,7 @@ if os.getenv('RTT_ROOT'):
 # EXEC_PATH is the compiler execute path, for example, CodeSourcery, Keil MDK, IAR
 if  CROSS_TOOL == 'gcc':
     PLATFORM    = 'gcc'
-    EXEC_PATH   = r'/opt/st/stm32cubeclt_1.21.0/GNU-tools-for-STM32/bin/arm-none-eabi-gcc'
+    EXEC_PATH   = r'C:\Users\XXYYZZ'
 elif CROSS_TOOL == 'keil':
     PLATFORM    = 'armcc'
     EXEC_PATH   = r'C:/Keil_v5'
