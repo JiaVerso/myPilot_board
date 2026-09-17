5c3a66f62af7762487cfe2e44cd0d14c9e2463d5
Jack Shaw <2870998241@qq.com>
2026-09-17 14:50:01 +0800

    update: my_math 数学库安全边界检查

--- bsp/qemu-vexpress-a9/applications/myPilot_lite/include/my_math.h
+++ bsp/qemu-vexpress-a9/applications/myPilot_lite/include/my_math.h
@@ -32,9 +32,6 @@ typedef struct{
 }Vector3f_t;
 
 float fast_invSqrt(float number);
-float math_vector_length(const float v[3]);
-float math_vector_dot(const float left[3],const float right[3]);
-void math_vector_cross(float result[3],const float left[3],const float right[3]);
 uint16_t math_crc16(uint16_t crc,const void * data,uint16_t len);
 void math_itoa(int32_t val,char * str);
 const char * math_afromi(int32_t val);
